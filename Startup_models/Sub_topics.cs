030e1950d0452d92f69a41f9a5182b702ca58faa
VibhavKapadia <vibhav@csu.fullerton.edu>
2020-10-11 05:04:22 -0700

    Add project files.

---
+++ Startup_models/Sub_topics.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Startup_models
+{
+    class Sub_topics
+    {
+        public virtual Topics Topics { get; set; }
+        public int Sub_topic_Id { get; set; }
+        public string Sub_topic_Name { get; set; }
+    }
+}
---
+++ Startup_models/Topics.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Startup_models
+{
+    public class Topics
+    {
+        public int TopicId { get; set; }
+        public string TopicName { get; set; }
+    }
+}
