6043a388a32a579af00634eca9f924bafcc1c830
VibhavKapadia <vibhav@csu.fullerton.edu>
2020-11-01 23:45:40 -0800

    footer to bottom

--- Startup_Frontend/wwwroot/js/topnav.js
+++ Startup_Frontend/wwwroot/js/topnav.js
@@ -31,12 +31,9 @@ function pageLoad() {
               document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
     });
-
     var mybutton = document.getElementById("myBtn");
-
     // When the user scrolls down 20px from the top of the document, show the button
     window.onscroll = function () { scrollFunction() };
-
     function scrollFunction() {
         if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
             mybutton.style.display = "block";
@@ -45,9 +42,5 @@ function pageLoad() {
         }
     }
 
-    //// When the user clicks on the button, scroll to the top of the document
-    //function gotop() {
-    //    document.body.scrollTop = 0;
-    //    document.documentElement.scrollTop = 0;
-    //}
 }
+
